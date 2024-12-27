852ffbb32fe062b1598c332d2b7332b951ae4b46
maior <maior@DESKTOP-FHGLER9>
2024-12-27 10:19:39 +0200

    Добавьте файлы проекта.

---
+++ PMK/Program.cs
@@ -0,0 +1,30 @@
+﻿
+class Program
+{
+    static void Main()
+    {
+        // Ввод данных
+        Console.WriteLine("Введите название учебного заведения:");
+        string institutionName = Console.ReadLine();
+
+        Console.WriteLine("Введите количество студентов, зачисленных на 1-й курс:");
+        int enrolledStudents = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите количество выпускников:");
+        int graduates = int.Parse(Console.ReadLine());
+
+        Console.WriteLine("Введите процент выпускников, которые работают по специальности:");
+        double percentageEmployed = double.Parse(Console.ReadLine());
+
+        // Расчеты
+        double q = (double)graduates / enrolledStudents;
+        double qp = percentageEmployed * q;
+
+        // Вывод результатов
+        Console.WriteLine($"Учебное заведение: {institutionName}");
+        Console.WriteLine($"Количество зачисленных студентов: {enrolledStudents}");
+        Console.WriteLine($"Количество выпускников: {graduates}");
+        Console.WriteLine($"Q (количество выпускников / количество зачисленных): {q:F2}");
+        Console.WriteLine($"Qp (процент выпускников, работающих по специальности * Q): {qp:F2}");
+    }
+}
